d04928a1d3769a31a6c1d46fe03187020206f031
Abtin Ravanipour <Abtin Ravanipour>
2017-02-18 18:09:57 -0800

    I can chose my hero

--- lotr.js
+++ lotr.js
@@ -13,15 +13,27 @@ function newGame() {
   hasWon = false;
   hasSpecial = true;
 
-  $(".yourChar, .yourOpp, .currOpp").empty();
+//   $(".yourChar, .yourOpp, .currOpp").empty();
 }
 
-  $(".caption").on("click", function(){
-  alert('test');
-  // if (heroChosen === false) {
-  //   yourHero = $(this).attr("data-name")
-  //   console.log(yourHero);
-  });
 
 
+  $(".thumbnail").on("click", function(){
+    console.log('test');
+  if (heroChosen === false) {
+    yourHero = $("<div>");
+    yourHero = $(this).attr("data-name")
+    console.log(yourHero);
+    $(" .yourChar").append(yourHero);
+    heroChosen = true;
+  }
+});
 
+
+var fridgeMagnet = $("<div>");
+      // 9. Give each "fridgeMagnet" the following classes: "letter fridge-color".
+      fridgeMagnet.addClass("letter fridge-color")
+      // 10. Then chain the following code onto the "fridgeMagnet" variable: .text($(this).attr("data-letter"))
+      fridgeMagnet.text($(this).attr("data-letter"))
+      // 11. Lastly append the fridgeMagnet variable to the "#display" div (provided);
+      $("#display").append(fridgeMagnet)
